cfe5de11d2c7966dbe287b31284bbaeaf51252a9
Jon Fletcher <jon.d.fletch@gmail.com>
2016-12-20 23:28:18 -0500

    Jon first

--- src/main/java/com/jfletcher/braingame/MainActivity.java
+++ src/main/java/com/jfletcher/braingame/MainActivity.java
@@ -1,10 +1,13 @@
 package com.jfletcher.braingame;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.*;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.CountDownTimer;
 import android.support.annotation.IntegerRes;
 import android.support.v7.app.AppCompatActivity;
@@ -14,40 +17,52 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    AlertDialog dialog;
     MyDBHandler myDBHandler;
     CountDownTimer timer;
     EditText userEditText;
-    String timerText,username,scoreText,viewText, message,mathText,
-        correctAnswerText,wrongAnswer1Text,wrongAnswer2Text,wrongAnswer3Text;
-    TextView field1, field2,field3,field4,mathTextView,scoreTextView,timerTextView;
+    String timerText, username, scoreText, viewText, message, mathText,
+            correctAnswerText, wrongAnswer1Text, wrongAnswer2Text, wrongAnswer3Text;
+    TextView field1, field2, field3, field4, mathTextView, scoreTextView, timerTextView;
     Button b, b2;
     int timerMax = 15;
-    int problemTotal,rn,cn,answer,wrongAnswer1,wrongAnswer2,wrongAnswer3,correctAnswer,points;
+    int problemTotal, rn, cn, answer, wrongAnswer1, wrongAnswer2, wrongAnswer3, correctAnswer, points;
     int threeInARow = 0;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
-    public void resetGame (View view){
+    public void resetGame(View view) {
 
         SQLiteDatabase db = myDBHandler.getWritableDatabase();
         myDBHandler.resetTable(db);
         printDatabase();
     }
 
-    public int randGen (int max, int min){
-        Random r = new Random ();
-        int nrn = r.nextInt(max - min +1)+min;
+    public int randGen(int max, int min) {
+        Random r = new Random();
+        int nrn = r.nextInt(max - min + 1) + min;
         return nrn;
     }
 
-    public boolean checkUnique (int wrongA){
+    public boolean checkUnique(int wrongA) {
 
         return wrongA == correctAnswer;
     }
 
-    public void setProblem () {
+    public void setProblem() {
 
         // Setting Random numbers and getting answer
         int randNum1 = randGen(6, 1);
@@ -55,7 +70,7 @@ public class MainActivity extends AppCompatActivity {
         int randNum2 = randGen(6, 1);
         correctAnswer = randNum1 + randNum2;
 
-        if (correctAnswer<10){
+        if (correctAnswer < 10) {
             correctAnswerText = "0" + Integer.toString(correctAnswer);
         } else {
             correctAnswerText = Integer.toString(correctAnswer);
@@ -67,58 +82,58 @@ public class MainActivity extends AppCompatActivity {
 
         //set one box to correct answer
         cn = randGen(4, 1);
-        if (cn == 1){
+        if (cn == 1) {
             field1.setText(correctAnswerText);
-        } else if (cn == 2){
+        } else if (cn == 2) {
             field2.setText(correctAnswerText);
-        } else if (cn == 3){
+        } else if (cn == 3) {
             field3.setText(correctAnswerText);
-        } else if (cn == 4){
+        } else if (cn == 4) {
             field4.setText(correctAnswerText);
         }
 
 
         //set other boxes to random numbers, re-randomize if equal to correctAnswer
-        wrongAnswer1 = randGen(12,2);
-        wrongAnswer2 = randGen(12,2);
-        wrongAnswer3 = randGen(12,2);
+        wrongAnswer1 = randGen(12, 2);
+        wrongAnswer2 = randGen(12, 2);
+        wrongAnswer3 = randGen(12, 2);
 
-        while (wrongAnswer1==correctAnswer){
-            wrongAnswer1 = randGen(12,2);
+        while (wrongAnswer1 == correctAnswer) {
+            wrongAnswer1 = randGen(12, 2);
         }
-        while (wrongAnswer2 == correctAnswer){
-            wrongAnswer2 = randGen(12,2);
+        while (wrongAnswer2 == correctAnswer) {
+            wrongAnswer2 = randGen(12, 2);
         }
-        while (wrongAnswer3 == correctAnswer){
-            wrongAnswer3 = randGen(12,2);
+        while (wrongAnswer3 == correctAnswer) {
+            wrongAnswer3 = randGen(12, 2);
         }
 
         //format single digits
-        if (wrongAnswer1<10){
+        if (wrongAnswer1 < 10) {
             wrongAnswer1Text = "0" + Integer.toString(wrongAnswer1);
         } else {
             wrongAnswer1Text = Integer.toString(wrongAnswer1);
         }
-        if (wrongAnswer2<10){
+        if (wrongAnswer2 < 10) {
             wrongAnswer2Text = "0" + Integer.toString(wrongAnswer2);
         } else {
             wrongAnswer2Text = Integer.toString(wrongAnswer2);
         }
-        if (wrongAnswer3<10){
+        if (wrongAnswer3 < 10) {
             wrongAnswer3Text = "0" + Integer.toString(wrongAnswer3);
         } else {
             wrongAnswer3Text = Integer.toString(wrongAnswer3);
         }
 
-        if (cn == 1){
+        if (cn == 1) {
             field2.setText(wrongAnswer1Text);
             field3.setText(wrongAnswer2Text);
             field4.setText(wrongAnswer3Text);
-        } else if (cn == 2){
+        } else if (cn == 2) {
             field1.setText(wrongAnswer1Text);
             field3.setText(wrongAnswer2Text);
             field4.setText(wrongAnswer3Text);
-        } else if (cn == 3){
+        } else if (cn == 3) {
             field1.setText(wrongAnswer1Text);
             field2.setText(wrongAnswer2Text);
             field4.setText(wrongAnswer3Text);
@@ -131,9 +146,9 @@ public class MainActivity extends AppCompatActivity {
         Log.i("math problem", mathText);
     }
 
-    public void setScore(){
+    public void setScore() {
 
-        if (checkAnswer()){
+        if (checkAnswer()) {
             //add point and to total
             points++;
             problemTotal++;
@@ -143,7 +158,7 @@ public class MainActivity extends AppCompatActivity {
             //add to total
             points--;
             problemTotal++;
-            threeInARow=0;
+            threeInARow = 0;
             Log.i("Answer", "False");
         }
         scoreText = Integer.toString(points) + "/" + Integer.toString(problemTotal);
@@ -155,8 +170,8 @@ public class MainActivity extends AppCompatActivity {
         return answer == correctAnswer;
     }
 
-    public void runTimer (int time){
-        timer = new CountDownTimer (time * 1000+30, 1000) {
+    public void runTimer(int time) {
+        timer = new CountDownTimer(time * 1000 + 30, 1000) {
 
             @Override
             public void onTick(long l) {
@@ -205,7 +220,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void selected(View view){
+    public void selected(View view) {
 
         // Finding TextField name and Text of box clicked
 
@@ -220,15 +235,15 @@ public class MainActivity extends AppCompatActivity {
         setScore();
         setProblem();
 
-        if (threeInARow==3){
+        if (threeInARow == 3) {
             timer.cancel();
-            threeInARow=0;
+            threeInARow = 0;
             String timeString = timerTextView.getText().toString();
             String[] separated = timeString.split(":");
             int newTime = Integer.parseInt(separated[1]);
             Log.i("new time", timerTextView.getText().toString());
             Log.i("Seconds", Integer.toString(newTime));
-            runTimer(newTime+3);
+            runTimer(newTime + 3);
         }
     }
 
@@ -253,7 +268,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //add to database
-    public void addToDB (){
+    public void addToDB() {
 
         username = userEditText.getText().toString();
 
@@ -263,13 +278,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // delete item
-    public void deleteScore(){
+    public void deleteScore() {
         String inputName = username;
         myDBHandler.deleteHighScore(inputName);
         printDatabase();
     }
 
-    public void printDatabase(){
+    public void printDatabase() {
         String c = myDBHandler.databaseToString();
         String s = myDBHandler.databaseToStringScore();
 
@@ -282,7 +297,7 @@ public class MainActivity extends AppCompatActivity {
         Log.i("DB Results", s);
     }
 
-    public void resetGame (){
+    public void resetGame() {
 
         field1.setText("00");
         field2.setText("00");
@@ -300,11 +315,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+        alertDialog.setTitle("Rules");
+        alertDialog.setMessage("Simple timed math game. Once the game starts, for every correct answer the player will get one point. " +
+                "For ever incorrect answer the player loses a point. If the player selects three correct answers in a row, they will get" +
+                " three extra seconds added to their time. Good luck!!");
+        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "Pos",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "Neg",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "Neut",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        alertDialog.show();
+
+//        Log.i("Dialog", dText);
+
         userEditText = (EditText) findViewById(R.id.userName);
 
         //set timer
         timerTextView = (TextView) findViewById(R.id.timer);
-         message = "00:" + Integer.toString(timerMax);
+        message = "00:" + Integer.toString(timerMax);
         timerTextView.setText(message);
         //set score
         scoreTextView = (TextView) findViewById(R.id.score);
@@ -324,7 +366,7 @@ public class MainActivity extends AppCompatActivity {
         field4.setEnabled(false);
 
         b = (Button) findViewById(R.id.playButton);
-        //b.setEnabled(false);
+        b.setEnabled(false);
 
         mathTextView = (TextView) findViewById(R.id.mathProblem);
 
@@ -332,4 +374,5 @@ public class MainActivity extends AppCompatActivity {
         myDBHandler = new MyDBHandler(this, null, null, 1);
         printDatabase();
     }
+
 }
